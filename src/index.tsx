4a248c65706575bedffb9b4bf39afe1e6ef44093
Lucas Pazelo <lucaspazello@gmail.com>
2023-05-19 07:22:57 -0400

    Importing some global css styles and creating a routing system

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import Login from './components/log-in';
 import Something from './components/something';
-import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './components/auth';
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 
+import 'primeflex/primeflex.css';
+import './index.css';
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+import "primereact/resources/primereact.min.css";                                       
+        
+
 const router = createBrowserRouter([
   {
-    path: "/login",
-    element: <Login />
-  },
-  {
     path: "/",
-    element: (
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    ),
+    element: <App />,
     children: [
       {
         path: "something",
         element: <Something />
       }
     ]
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ]);
 
@@ -37,11 +37,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
